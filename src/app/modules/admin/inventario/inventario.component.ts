9bda574103837191886cd82702ce413aefdcb912
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-04-17 19:43:05 -0300

    implementando função para obter o item selecioando para o usuário

--- src/app/modules/admin/inventario/inventario.component.ts
+++ src/app/modules/admin/inventario/inventario.component.ts
@@ -1,30 +1,50 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import {
+    Component,
+    OnInit,
+    ViewChild,
+    OnDestroy,
+    Input,
+    EventEmitter,
+    Output,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { InventarioService } from './inventario.service';
 import { Remedio } from 'app/model/Remedio';
+
 // import 'moment-timezone';
 
 @Component({
-  selector: 'app-teste',
-  templateUrl: './inventario.component.html',
-  styleUrls: ['./inventario.component.scss']
+    selector: 'app-teste',
+    templateUrl: './inventario.component.html',
+    styleUrls: ['./inventario.component.scss'],
 })
 export class InventarioComponent implements OnInit {
+    @Input() cartTotal: number;
+    itens: Remedio[] = [];
+    @Output() cartItemDeleted = new EventEmitter<{
+        productId: number;
+    }>();
+    @Output() cartItemChanged = new EventEmitter<{
+        productId: number;
+    }>();
+
     originalRemediosList: Remedio[] = [];
     remediosList: MatTableDataSource<Remedio>;
 
     @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-    displayedColumns: string[] = ['nomeMedicamento', 'dosagem', 'estoque', 'imagem' , 'actions'];
+    displayedColumns: string[] = [
+        'nomeMedicamento',
+        'dosagem',
+        'estoque',
+        'imagem',
+        'actions',
+    ];
 
     searchText: string = '';
 
-    constructor(
-        private _dialog: MatDialog,
-        private _service: InventarioService
-    ) {}
+    constructor(private _service: InventarioService) {}
 
     ngOnInit(): void {
         this._service._remedios$.subscribe((response) => {
@@ -53,11 +73,20 @@ export class InventarioComponent implements OnInit {
         }
     }
 
-    // openDialog(): void {
-    //     const dialogRef = this._dialog.open(CertificateCreateComponent, {
-    //         width: '100%',
-    //         height: 'auto',
-    //         maxHeight: '95vh',
-    //     });
-    // }
+    onCartItemDeleted(productData: { productId: number }) {
+        this.cartItemDeleted.emit({
+            productId: productData.productId,
+        });
+    }
+
+    onCartItemChanged(productData: { productId: number }) {
+        this.cartItemChanged.emit({
+            productId: productData.productId,
+        });
+    }
+
+    retirarRemedio(itemRetirado: Remedio) {
+        console.log(itemRetirado);
+        this.itens.push(itemRetirado);
+    }
 }

2e371c2d3fe1bf6fd18522d2ae374e1eb56cda26
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-04-17 19:41:11 -0300

    construindo um componente abstraído para receber e listar os itens selecionados que serão retirados

--- src/app/modules/admin/inventario/item/item.component.ts
+++ src/app/modules/admin/inventario/item/item.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import { Remedio } from 'app/model/Remedio';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit {
+        @Input() item: Remedio;
+        @Output() itemDeletedo = new EventEmitter<{
+          productId: number
+        }>();
+        @Output() cartItemChanged = new EventEmitter<{
+          productId: number
+        }>();
 
-}
+        onItemDeleted(event) {
+          const id = event.target.getAttribute('id');
+          this.itemDeletedo.emit({
+              productId: id
+            });
+        }
+
+        onItemChanged(event) {
+          const id = event.target.getAttribute('id');
+          this.cartItemChanged.emit({
+              productId: id
+            });
+        }
+
+        constructor() {
+         }
+
+        ngOnInit() {
+
+        }
+
+
+      }
+
